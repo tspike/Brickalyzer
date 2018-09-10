5c94d1c5977aedae17cbc7f3149f04cf422cca59
Tres Spicher <tres@tspike.com>
2018-09-09 18:32:14 -0700

    Initial Commit

---
+++ Brickalyzer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Brickalyzer
+//
+//  Created by Tres Spicher on 9/9/18.
+//  Copyright © 2018 Spike Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
